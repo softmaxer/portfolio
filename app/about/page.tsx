215a454708ba28f5374c2bf67a3d743bebc92612
softmaxer <sriram.vadlamani@proton.me>
2024-06-03 19:23:41 +0000

    Add button to redirect back to homepage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 export default function AboutPage() {
   const aboutDesc: String =
     "I'm an AI engineer / software developper from India, currently living in france and I like building apps.";
   return (
-    <div className="flex flex-col items-center justify-center min-h-64 space-x-2">
-      <h1 className="text-2xl text-black">About me</h1>
-      <p className="text-black">{aboutDesc}</p>
+    <div className="flex flex-col items-center justify-between min-h-64">
+      <h1 className="text-3xl text-black">Who am I?</h1>
+      <div className="flex flex-row max-w-screen-sm items-center">
+        <Image src={"/dp.png"} alt="profile_photo" height={150} width={150} />
+        <p className="text-black font-semibold">{aboutDesc}</p>
+      </div>
     </div>
   );
 }
