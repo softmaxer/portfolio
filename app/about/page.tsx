3a8a1f4a0012ca78b25edcd8d96e70c356ea9d23
softmaxer <sriram.vadlamani@proton.me>
2024-06-04 14:34:45 +0000

    Change text size

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,10 +5,10 @@ export default function AboutPage() {
     "I'm an AI engineer / software developper from India, currently living in france and I like building apps.";
   return (
     <div className="flex flex-col items-center justify-between min-h-64">
-      <h1 className="text-3xl text-black">Who am I?</h1>
+      <h1 className="text-3xl ">Who am I?</h1>
       <div className="flex flex-row max-w-screen-sm items-center">
         <Image src={"/dp.png"} alt="profile_photo" height={150} width={150} />
-        <p className="text-black font-semibold">{aboutDesc}</p>
+        <p className=" font-semibold">{aboutDesc}</p>
       </div>
     </div>
   );
