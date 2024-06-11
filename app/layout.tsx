1e34354f1b39fb5165a249e132e47f64ef4c8f6a
softmaxer <sriram.vadlamani@proton.me>
2024-06-11 19:14:29 +0200

    Remove header from layout for redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
-          <Header />
           <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
         </div>
       </body>
