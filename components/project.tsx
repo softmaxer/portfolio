266da9838a642ab3e13c9655b13cea630f0673e9
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 00:36:36 +0200

    Add on hover CSS to project component

--- components/project.tsx
+++ components/project.tsx
@@ -1,12 +1,19 @@
-import Link from "next/link";
 import { ReactNode } from "react";
 
 export function createProject(name: string, description: string): ReactNode {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>{name}</h1>
-      <h3>{description}</h3>
-      <Link href={"https://www.github.com/softmaxer/" + name}>Github</Link>
-    </div>
+    <a
+      href="https://www.github.com/softmaxer/osail"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2 className="mb-3 text-2xl font-semibold">
+        {name}{" "}
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+      </h2>
+    </a>
   );
 }
