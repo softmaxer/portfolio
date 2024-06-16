e027f30b82d58d67383843435549833d2cdf562f
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 16:36:11 +0200

    redender button with subtle hover animation

--- components/buttons/redirect.tsx
+++ components/buttons/redirect.tsx
@@ -8,15 +8,12 @@ export default function RedirectTo({ page, destination }: PageInfo) {
     <div className="flex flex-col items-center justify-center">
       <a
         href={destination}
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:underline"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <h2 className="mb-3 text-2xl font-semibold text-center">
+        <h2 className="mb-3 text-2xl font-semibold text-center inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           {page}{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
         </h2>
       </a>
     </div>
