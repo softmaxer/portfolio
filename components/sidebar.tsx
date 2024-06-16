327cf466ca9c12beb6436eafc802148120d2bb5f
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 17:47:33 +0200

    Revisit flex boxes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,17 +5,13 @@ import RedirectTo from "./buttons/redirect";
 export default function Sidebar() {
   return (
     <>
-      <div className="flex">
-        <div className="bg-gray-800">
-          <div className="text-white flex flex-col fixed min-h-screen transition-all justify-center items-center">
-            <RedirectTo page="About" destination="/about" />
-            <RedirectTo
-              page="Contact"
-              destination="mailto:sriram.vadlamani@proton.me"
-            />
-            <RedirectTo page="Blog" destination="/blog" />
-          </div>
-        </div>
+      <div className="text-white flex flex-col fixed min-h-screen transition-all justify-center items-center">
+        <RedirectTo page="About" destination="/about" />
+        <RedirectTo
+          page="Contact"
+          destination="mailto:sriram.vadlamani@proton.me"
+        />
+        <RedirectTo page="Blog" destination="/blog" />
       </div>
     </>
   );
