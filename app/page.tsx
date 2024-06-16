df7c1cd6995cd2a312c882f439662af2c5b106d0
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 15:00:09 +0200

    Redesign app, restart from nextJS boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import LandingPage from "@/components/landing";
+import RedirectTo from "@/components/buttons/redirect";
 
 export default function Home() {
-  return <LandingPage />;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-7xl lg:text-left text-center">Sriram Vadlamani</p>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
+        <RedirectTo page="About" destination="/about" />
+        <RedirectTo
+          page="Hire me"
+          destination="mailto:sriram.vadlamani@proton.me"
+        />
+        <RedirectTo page="Blog" destination="/blog" />
+      </div>
+    </main>
+  );
 }
