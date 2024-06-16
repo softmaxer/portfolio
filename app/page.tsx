314f8f31f4f0417aff3b746ee342b5be37c70cd0
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 17:47:14 +0200

    Change principal accent color

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ export default function Home() {
         <p>
           Hey <span>👋</span>! My name is
         </p>
-        <p className="text-7xl lg:text-left text-center">Sriram Vadlamani</p>
+        <p className="text-7xl lg:text-left text-center text-sky-500">
+          Sriram Vadlamani
+        </p>
       </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left"></div>
     </>
   );
 }
