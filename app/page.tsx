83a3498ffade5bcfe86b29ea5ae0116d47d4828d
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 16:35:50 +0200

    Add a title div for my name

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import RedirectTo from "@/components/buttons/redirect";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-7xl lg:text-left text-center">Sriram Vadlamani</p>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
-        <RedirectTo page="About" destination="/about" />
-        <RedirectTo
-          page="Hire me"
-          destination="mailto:sriram.vadlamani@proton.me"
-        />
-        <RedirectTo page="Blog" destination="/blog" />
+    <>
+      <div className="flex flex-col">
+        <p>
+          Hey <span>👋</span>! My name is
+        </p>
+        <p className="text-7xl lg:text-left text-center">Sriram Vadlamani</p>
       </div>
-    </main>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left"></div>
+    </>
   );
 }
