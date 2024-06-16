597c449a85c3a7d7cff8c627175fea41c5c06654
softmaxer <sriram.vadlamani@proton.me>
2024-06-16 17:46:55 +0200

    Add an about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <h1 className="text-sky-500 text-7xl">About me</h1>
+        <p className="text-xl lg:text-left text-center">
+          I am a developper, currently based in france.
+        </p>
+      </div>
+    </>
+  );
+}
