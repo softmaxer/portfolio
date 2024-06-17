79fc1e061c8ca57aaf8c9c67fde100898c912967
Sriram Vadlamani <sriram.vadlamani@proton.me>
2024-06-17 14:20:08 +0200

    Change offset of main class to dropdown content start offset

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,7 +5,7 @@ import RedirectTo from "./buttons/redirect";
 export default function Sidebar() {
   return (
     <>
-      <div className="text-white flex flex-row fixed  transition-all justify-center items-center">
+      <div className="text-white flex flex-row fixed transition-all justify-center items-center">
         <RedirectTo page="Home" destination="/" />
         <RedirectTo page="About" destination="/about" />
         <RedirectTo
