35a3ce9d1cd93118ef2aa6f44968a2d4c8bd7572
Sriram Vadlamani <sriram.vadlamani@proton.me>
2024-06-17 14:36:14 +0200

    Add gap to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 export default function About() {
   return (
     <>
-      <div className="flex flex-col">
-        <h1 className="text-orange-300 text-7xl">About me</h1>
-        <p className="text-xl lg:text-left text-center">
-          I am a developper, currently based in france.
-        </p>
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between gap-20">
+          <h1 className="text-orange-300 text-7xl">About me</h1>
+          <p className="text-xl lg:text-left text-center">
+            I am a developper, currently based in france.
+          </p>
+        </div>
       </div>
     </>
   );
