009b0f4956eea9162738bb2d05dd837d9d9264a8
softmaxer <sriram.vadlamani@proton.me>
2024-06-17 22:42:04 +0200

    Add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import ProjectCard from "@/components/project-card";
+
+export default function Projects() {
+  return (
+    <ProjectCard
+      name="osail"
+      description="A local leaderboard for evaluating large language models"
+      link="https://www.github.com/softmaxer/osail"
+    />
+  );
+}
