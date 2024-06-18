fb7305959c9f37142a66c7c36c7c81c42780b5ea
Sriram Vadlamani <sriram.vadlamani@proton.me>
2024-06-18 08:44:32 +0200

    Add projects to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,26 @@ import ProjectCard from "@/components/project-card";
 
 export default function Projects() {
   return (
-    <ProjectCard
-      name="osail"
-      description="A local leaderboard for evaluating large language models"
-      link="https://www.github.com/softmaxer/osail"
-    />
+    <>
+      <div className="flex flex-col">
+        <p className="text-7xl lg:text-left  text-orange-300 text-center">
+          My projects
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <ProjectCard
+          name="OSAIL"
+          description="A local leaderboard for evaluating LLMs"
+          link="https://www.github.com/softmaxer/osail"
+          image="/cards/osail.png"
+        />
+        <ProjectCard
+          name="Envy"
+          description="A CLI tool to manage your .env files"
+          link="https://www.github.com/softmaxer/envy"
+          image="/cards/envy.png"
+        />
+      </div>
+    </>
   );
 }
