d36195138adf2ac56e2d5701bcee98bd5bfc3b19
softmaxer <sriram.vadlamani@proton.me>
2024-06-18 21:44:14 +0200

    Complete projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,11 +2,9 @@ import ProjectCard from "@/components/project-card";
 
 export default function Projects() {
   return (
-    <>
-      <div className="flex flex-col">
-        <h1 className="text-orange-300 text-7xl">My projects</h1>
-      </div>
-      <div className="flex flex-col">
+    <div className="flex flex-col justify-between items-center gap-20">
+      <h1 className="text-orange-300 text-7xl">My projects</h1>
+      <div className="flex flex-row justify-between items-center gap-4">
         <ProjectCard
           name="OSAIL"
           description="A local leaderboard for evaluating LLMs"
@@ -19,7 +17,25 @@ export default function Projects() {
           link="https://www.github.com/softmaxer/envy"
           image="/cards/envy.png"
         />
+        <ProjectCard
+          name="Home Credit Default Risk"
+          description="An MLOps gig to predict if a client can pay their loan back."
+          link="https://github.com/softmaxer/openclassrooms-ds-p7"
+          image="/cards/home-credit.jpg"
+        />
+        <ProjectCard
+          name="Fruits 360"
+          description="Deploying an image feature extraction model on AWS with PySpark"
+          link="https://github.com/softmaxer/fruits-360-cloud"
+          image="/cards/fruits.png"
+        />
+        <ProjectCard
+          name="Portfolio"
+          description="This website!"
+          link="softmaxer.github.io"
+          image="/icons/profile_pic.png"
+        />
       </div>
-    </>
+    </div>
   );
 }
--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
-import ProfileButton from "./buttons/profile-button";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export interface ProjectCardProps {
   name: string;
@@ -15,25 +16,22 @@ export default function ProjectCard({
   image,
 }: ProjectCardProps) {
   return (
-    <a
-      href={link}
-      className="flex flex-col items-center bg-white border border-white-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-transparent"
-    >
-      <Image
-        src={image ? image : "/cards/default.png"}
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-        width={200}
-        height={100}
-        alt={name}
-      />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {name}
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {description}
-        </p>
+    <div className="flex flex-col justify-evenly items-center hover:scale-125 translate-y-1 transition-all group">
+      <a
+        href={link}
+        className="flex flex-col items-center  rounded-lg    dark:bg-transparent"
+      >
+        <Image
+          src={image ? image : "/cards/default.png"}
+          className="object-cover  rounded-lg h-96 md:h-auto md:w-32 "
+          width={200}
+          height={100}
+          alt={name}
+        />
+      </a>
+      <div className="hidden group-hover:block bg-transparent rounded-lg w-32">
+        <p className="text-white">{description}</p>
       </div>
-    </a>
+    </div>
   );
 }
